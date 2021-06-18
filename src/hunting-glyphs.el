2c71f47ae5f61c6922a89b297f5fb3fc5c469dd4
Joshua Person <person@Joshuas-MacBook-Pro.local>
2021-06-17 21:48:22 -0500

    Glyphs PoC

--- src/hunting-glyphs.el
+++ src/hunting-glyphs.el
@@ -17,17 +17,23 @@
 ;;
 ;;  description
 ;;
+;;;Warning: Major mode commands must not call font-lock-add-keywords under any circumstances, either directly or indirectly, except through their mode hooks. (Doing so would lead to incorrect behavior for some minor modes.) They should set up their rules for search-based fontification by setting font-lock-keywords.
 ;;;
 
+(define-minor-mode hunting-glyph-mode
+  "Hunting Glyph mode is a minor mode that enables glyphs"
+  nil "blah" nil
 
+  (add-to-list 'font-lock-extra-managed-props 'display)
+  (loop for buf in (delete-dups (mapcar 'car hunting-glyph-hooks))
+        do (font-lock-add-keywords nil `((,buf 1  `(face nil display ,(hunting-glyphify (match-string 1) ,buf))))))
 
-(define-minor-mode hunting-mode
-  "Provides tools and highlighting for"
-  nil " hunting" nil ; No modeline lighter - it's already obvious when the mode is on.
-  (when hunting-mode (font-lock-add-keywords nil '((hunting-mode--find-date-match 1 'font-lock-warning-face t t)))
-	(font-lock-add-keywords nil '((hunting-mode--in-feed-match 0 'font-lock-constant-face t t)))
-	(font-lock-fontify-buffer))
-  )
+  (if (fboundp 'font-lock-flush)
+      (font-lock-flush)
+    (when font-lock-mode
+      (with-no-warnings (font-lock-fontify-buffer)))))
+
+(defvar hunting-glyph-debugging t)
 
 (defun in-vt (hash apikey)
   (let ((result nil))
@@ -44,17 +50,29 @@
       )
     (if (= (alist-get 'response_code result) 1) t)))
 
-;; (require 'ov)
+(defun hunting-foo (thing)
+  t)
 
-;; (ov-set (ov-regexp "\\([0123456789abcdefABCDEF]\\{40\\} \\)")
-;;         'before-string "yeet!")
+;; Register new hook
+;; (regex funtion t-glypht f-glyph)
+(defvar hunting-glyph-hooks
+  '(("\\([0123456789abcdefABCDEFG]\\{40\\} \\)" hunting-foo "T" "F")
+    ("\\([0123456789abcdefABCDEFG]\\{40\\} \\)" hunting-foo "Z" "G")
+    ))
 
-;; (ov-set "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" 'before-string ">>")
+;; Cache Format (object function glyph timestamp)
+(defvar hunting-search-cache '())
 
-;; (add-to-list 'font-lock-extra-managed-props 'display)
-;; (font-lock-add-keywords nil '((" \\(/\\) " 1 '(face nil display "÷"))))
-;; (font-lock-add-keywords nil '(("\\([0123456789abcdefABCDEF]\\{40\\} \\)" 1  `(face nil display ,(format "✔ %s" (match-string 1))))))
-;; https://github.com/emacsorphanage/ov
+(defun hunting-glyphify (match regex)
+  (let ((cache-hit (car (seq-filter (lambda (elt) (string= match (car elt))) hunting-search-cache))))
+    (if cache-hit (format "%s%s" (nth 2 cache-hit) match)
+      (let* ((glyphs (seq-filter (lambda (elt) (string= (car elt) regex)) hunting-glyph-hooks))
+             (glyph-values (mapcar (lambda (elt) (if (funcall (nth 1 elt) match) (nth 2 elt) (nth 3 elt))) hunting-glyph-hooks)))
+        (message (format "Caching %s" match))
+        (add-to-list 'hunting-search-cache `(,match nil ,(apply #'concat glyph-values) ,(float-time)))
+        (hunting-glyphify match regex)
+        ))))
+
 
 (provide 'hunting-glyphs)
 ;;; Code:
