15d1e78759ec4103aa9a17513a8bea6dbe7fa8c2
noonker <ceo@legitimate.company>
2024-08-27 23:11:09 -0500

    Convert between Radare2 and Yara hex

--- lisp/hunting-binary.el
+++ lisp/hunting-binary.el
@@ -20,6 +20,8 @@
 ;;
 ;;; Code:
 
+(require 'dash)
+
 (defun hunting-binary-addr-on-screen ()
   "Find all the things on the current page that look like a binary address."
   (let ((view (buffer-substring-no-properties
@@ -40,6 +42,51 @@
    (completing-read "Choose an address: " (hunting-binary-addr-on-screen)))
   (execute-kbd-macro (read-kbd-macro "C-y"))
   )
+
+(defun hunting-binary-yara-to-radare (yara-string)
+  "Convert Yara formatted rule YARA-STRING to radare2 format."
+  (->> yara-string
+       (string-replace "?" ".")
+       (string-replace " " "")
+       (string-replace "\n" "")))
+
+(defun hunting-binary-radare-to-yara (radare-string)
+  "Convert Radare2 formatted RADARE-STRING to Yara format."
+  (let ((replaced (string-replace "." "?" radare-string)))
+    (let ((i 1)
+	  (replaced (split-string replaced "" t))
+	  (result ""))
+      (while (< i (length replaced))
+	(if (= 0 (% i 2))
+	    (setq result (concat result
+				 (nth i replaced)
+				 " "))
+	  (setq result (concat result
+			       (nth i replaced))))
+	(if (= 0 (% i 8))
+	    (setq result (concat result "\n")))
+	(setq i (+ 1 i)))
+      result)))
+
+(defun hunting-binary-radare-to-yara-region ()
+  "Convert the radare search string at region to yara bytes."
+  (interactive)
+  (let ((radare-string (buffer-substring-no-properties
+			(region-beginning)
+			(region-end))))
+    (kill-region (region-beginning)
+		 (region-end))
+    (insert (hunting-binary-radare-to-yara radare-string))))
+
+(defun hunting-binary-yara-to-radare-region ()
+  "Convert the yara bytes at region to radare search string."
+  (interactive)
+  (let ((radare-string (buffer-substring-no-properties
+			(region-beginning)
+			(region-end))))
+    (kill-region (region-beginning)
+		 (region-end))
+    (insert (hunting-binary-yara-to-radare radare-string))))
 
 (provide 'hunting-binary)
 
